3ae93c64a3c8c45236b16d8fa2c7f7d2070a77ee
Dang Linh Anh <70094015+anhdang000@users.noreply.github.com>
2023-03-23 11:27:48 +0700

    Update server.py

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ from fastapi import UploadFile, File, Form
 from starlette.responses import StreamingResponse
 from pymongo import MongoClient
 import json
-# from model_configs import *
-# from demo import inference
+from model_configs import *
+from demo import inference
 
 # Database
 client = MongoClient(host='mongodb',
@@ -47,55 +47,55 @@ def get_stored_images():
             db.close()
 
 
-# @app.post('/challenge')
-# def predict(challenge: str = Form(...), input: UploadFile = File(...)):
-#     # Check challenge name
-#     if challenge != 'cv3':
-#         return {"message": "The API only works for challenge `cv3`"}
+@app.post('/challenge')
+def predict(challenge: str = Form(...), input: UploadFile = File(...)):
+    # Check challenge name
+    if challenge != 'cv3':
+        return {"message": "The API only works for challenge `cv3`"}
 
-#     # Check file format
-#     file_ext = input.filename.split('.')[-1]
-#     supported_image_formats = ['bmp', 'jpg', 'jpeg', 'jp2', 'png', 'tiff', 'webp', 'xbm']
-#     if file_ext not in supported_image_formats:
-#         return {
-#             "error": "Cannot read the imported file. Please refer to our supported image formats", 
-#             "supported_image_formats": str(supported_image_formats)
-#             }
+    # Check file format
+    file_ext = input.filename.split('.')[-1]
+    supported_image_formats = ['bmp', 'jpg', 'jpeg', 'jp2', 'png', 'tiff', 'webp', 'xbm']
+    if file_ext not in supported_image_formats:
+        return {
+            "error": "Cannot read the imported file. Please refer to our supported image formats", 
+            "supported_image_formats": str(supported_image_formats)
+            }
 
-#     # Check file validation
-#     try:
-#         image = Image.open(input.file).convert('RGB')
-#     except:
-#         return {"error": f"File {input.filename} could not be read"}
+    # Check file validation
+    try:
+        image = Image.open(input.file).convert('RGB')
+    except:
+        return {"error": f"File {input.filename} could not be read"}
 
-#     if image is None:
-#         return {"error": f"File {input.filename} could not be read"}
+    if image is None:
+        return {"error": f"File {input.filename} could not be read"}
     
-#     image = np.array(image)
-#     image, raw_image = preprocessing(image, device, CONFIG)
-#     labelmap = inference(model, image, raw_image, postprocessor)
-#     labels = np.unique(labelmap)
+    image = np.array(image)
+    image, raw_image = preprocessing(image, device, CONFIG)
+    labelmap = inference(model, image, raw_image, postprocessor)
+    labels = np.unique(labelmap)
 
-#     mask = np.zeros((raw_image.shape[0], raw_image.shape[1]))
-#     for i, label in enumerate(labels):
-#         mask_inclass = labelmap == label
+    mask = np.zeros((raw_image.shape[0], raw_image.shape[1]))
+    for i, label in enumerate(labels):
+        mask_inclass = labelmap == label
 
-#         class_name = classes[label]
-#         if class_name in classes_of_interest:
-#             mask += mask_inclass
+        class_name = classes[label]
+        if class_name in classes_of_interest:
+            mask += mask_inclass
     
-#     # Create mask
-#     mask = np.where(mask >= 1, 1, mask)
-#     mask = (mask * 255).astype(np.uint8)
-#     if np.max(mask) == 0:
-#         return {"warning": "Could not find any subjects in the image"}
-#     else:
-#         mask_3d = np.stack([mask]*3, axis=2)
+    # Create mask
+    mask = np.where(mask >= 1, 1, mask)
+    mask = (mask * 255).astype(np.uint8)
+    if np.max(mask) == 0:
+        return {"warning": "Could not find any subjects in the image"}
+    else:
+        mask_3d = np.stack([mask]*3, axis=2)
 
-#     # Apply with source
-#     raw_rgb = raw_image[:, :, ::-1]
-#     output = np.where(mask_3d == 255, raw_rgb, 255)
+    # Apply with source
+    raw_rgb = raw_image[:, :, ::-1]
+    output = np.where(mask_3d == 255, raw_rgb, 255)
 
-#     _, output_png = cv2.imencode('.png', output)
+    _, output_png = cv2.imencode('.png', output)
 
-#     return StreamingResponse(io.BytesIO(output_png.tobytes()), media_type="image/png")
+    return StreamingResponse(io.BytesIO(output_png.tobytes()), media_type="image/png")
